581f033253de6845cfeae1b8ef322800c5f78f7f
MALGOJ <manuelgomezjimenez304@gmail.com>
2024-10-28 16:34:13 -0500

    initial project

---
+++ src/main/kotlin/org/example/colabtasksapi/CoLabTasksApiApplication.kt
@@ -0,0 +1,11 @@
+package org.example.colabtasksapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoLabTasksApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<CoLabTasksApiApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/colabtasksapi/CoLabTasksApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.colabtasksapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoLabTasksApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
